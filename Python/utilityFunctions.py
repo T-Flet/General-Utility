ff3fd9d4cb11e26943b28e0aaddc12da0191ecd3
T-Flet <T-Fletcher@outlook.com>
2019-11-11 16:49:15 +0000

    Added a Python folder and a script containing a neat unique (elements filter)

---
+++ Python/utilityFunctions.py
@@ -0,0 +1,4 @@
+
+def unique(xs):
+    seen = [] # Note: 'in' tests x is z or x == z, hence it works with __eq__ overloading
+    return [x for x in xs if x not in seen and not seen.append(x)] # Neat short-circuit 'and' trick
