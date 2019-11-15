a12638126b44c20ad2e2b48cbeda1aaa22e37468
T-Flet <T-Fletcher@outlook.com>
2019-11-15 16:44:06 +0000

    Added chunk function to Python

--- Python/utilityFunctions.py
+++ Python/utilityFunctions.py
@@ -8,3 +8,7 @@ def flatten(list_of_lists):
 def unique(xs):
     seen = [] # Note: 'in' tests x is z or x == z, hence it works with __eq__ overloading
     return [x for x in xs if x not in seen and not seen.append(x)] # Neat short-circuit 'and' trick
+
+
+def chunk(xs, n):
+    return (xs[i:i + n] for i in range(0, len(xs), n))
