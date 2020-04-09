ea06bbb4ed7a8f8bf6f437e8a38495f0450d3d3f
T-Flet <T-Fletcher@outlook.com>
2020-04-09 14:07:36 +0100

    Restructured Python functions for consistency w.r.t. hashable vs unhashable elements collections

--- Python/utilityFunctions.py
+++ Python/utilityFunctions.py
@@ -2,17 +2,10 @@ from itertools import chain
 from functools import reduce
 
 
-def flatten(list_of_lists):
-    return list(chain.from_iterable(list_of_lists))
+# Note: versions of functions with the '_h' suffix only work and are optimal for collections of hashable elements
 
 
-def unique(xs):
-    seen = [] # Note: 'in' tests x is z or x == z, hence it works with __eq__ overloading
-    return [x for x in xs if x not in seen and not seen.append(x)] # Neat short-circuit 'and' trick
-
-
-def chunk(xs, n):
-    return (xs[i:i + n] for i in range(0, len(xs), n))
+def flatten(list_of_lists): return list(chain.from_iterable(list_of_lists))
 
 
 def partition(p, xs): # Haskell's partition function: partition p xs == (filter p xs, filter (not . p) xs)
@@ -22,19 +15,31 @@ def partition(p, xs): # Haskell's partition function: partition p xs == (filter 
     return reduce(select, xs, ([],[]))
 
 
-def lists_of_unhashables__eq(xs, ys):
+def unique(xs):
+    seen = [] # Note: 'in' tests x is z or x == z, hence it works with __eq__ overloading
+    return [x for x in xs if x not in seen and not seen.append(x)] # Neat short-circuit 'and' trick
+def unique_h(xs): return type(xs)(set(xs))
+
+
+def eq(xs, ys):
     cys = list(ys) # make a mutable copy
     try:
         for x in xs: cys.remove(x)
     except ValueError: return False
     return not cys
+def eq_h(xs, ys): return xs == ys # Pointless, but here for completeness
 
-def lists_of_unhashables__diff(xs, ys):
+
+def diff(xs, ys):
     cxs = list(xs) # make a mutable copy
     try:
         for y in ys: cxs.remove(y)
     except ValueError: pass
     return cxs
+def diff_h(xs, ys): return type(xs)(set(xs) - set(ys))
+
+
+def chunk(xs, n): return (xs[i:i + n] for i in range(0, len(xs), n))
 
 
 def interval_overlap(a, b): # Two interval tuples
@@ -46,3 +51,5 @@ def interval_overlap(a, b): # Two interval tuples
 def update_dict_with(d0, d1, f):
     for k, v in d1.items(): d0[k] = f(d0[k], v) if k in d0 else v
     return d0
+
+
